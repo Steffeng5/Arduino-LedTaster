84d8fc3e971e27c18f9592dbcd510b53c01e92bc
Steffen Genauck <Steffen@klabuster.com>
2016-09-30 17:38:00 +0200

    use internal pullup for button

--- LedTaster.ino
+++ LedTaster.ino
@@ -1,14 +1,14 @@
 
-int inPin = 12;        
+int inPin = 12;
 int ledRingPin = 5;
 
 int redPin = 3;
 int greenPin = 9;
 int bluePin = 10;
 
-int ledRingState = HIGH;   
-int reading;          
-int previous = LOW;   
+int ledRingState = HIGH;
+int reading;
+int previous = LOW;
 int mode = 0;
 boolean pressed = false;
 boolean longPressed = false;
@@ -42,44 +42,32 @@ int* colors[20];
 
 void setup()
 {
-  pinMode(inPin, INPUT);
+  pinMode(inPin, INPUT_PULLUP);
   pinMode(ledRingPin, OUTPUT);
   pinMode(redPin, INPUT);
   pinMode(greenPin, INPUT);
-  pinMode(bluePin, INPUT); 
-  
-colors[0] = color0;
-colors[1] = color1;
-colors[2] = color2;
-colors[3] = color3;
-colors[4] = color4;
-colors[5] = color5;
-colors[6] = color6;
-colors[7] = color7;
-colors[8] = color8;
-colors[9] = color9;
-colors[10] = color10;
-colors[11] = color11;
-colors[12] = color12;
-colors[13] = color13;
-colors[14] = color14;
-colors[15] = color15;
-colors[16] = color16;
-colors[17] = color17;
+  pinMode(bluePin, INPUT);
 
- 
- 
- 
- 
- 
- 
- 
- 
-   Serial.begin(9600);
-   
-   
-   
- 
+  colors[0] = color0;
+  colors[1] = color1;
+  colors[2] = color2;
+  colors[3] = color3;
+  colors[4] = color4;
+  colors[5] = color5;
+  colors[6] = color6;
+  colors[7] = color7;
+  colors[8] = color8;
+  colors[9] = color9;
+  colors[10] = color10;
+  colors[11] = color11;
+  colors[12] = color12;
+  colors[13] = color13;
+  colors[14] = color14;
+  colors[15] = color15;
+  colors[16] = color16;
+  colors[17] = color17;
+
+  Serial.begin(9600);
 }
 
 void loop()
@@ -88,48 +76,48 @@ void loop()
   pressed = false;
   reading = digitalRead(inPin);
 
+  while (digitalRead(inPin) == HIGH && !longPressed && millis() - time > debounce) {
+    pressed = true;
+    longPressed = false;
+    debounce = 200;
 
-    while (digitalRead(inPin) == HIGH && !longPressed && millis() - time > debounce) {
-      pressed = true;
-      longPressed = false;
-      debounce = 200;
-      
-      if (millis() - lastLoop > 1000) {    
-        longPressed = true;
-        debounce = 1000;
-      }
+    if (millis() - lastLoop > 1000) {
+      longPressed = true;
+      debounce = 1000;
     }
+  }
 
-  
+
 
   if (pressed && millis() - time > debounce) {
     if (longPressed) {
+      Serial.println("long pressed");
       mode = 0;
       ledRingState = HIGH;
       longPressed = false;
     } else {
+      Serial.print("short pressed, Mode: ");
+      Serial.println(mode);
       ledRingState = LOW;
-        mode++;
-        if (mode == 18) mode = 1;
-        Serial.println(mode);
-
-    }
-      
-      time = millis();
+      mode++;
+      if (mode == 18) mode = 1;
     }
 
-  
-  
+    time = millis();
+  }
+
+
+
   setLedIntensity(redPin, colors[mode][0]);
   setLedIntensity(greenPin, colors[mode][1]);
-  setLedIntensity(bluePin, colors[mode][2]);  
- 
+  setLedIntensity(bluePin, colors[mode][2]);
+
   digitalWrite(ledRingPin, ledRingState);
-  
-  
+
+
   previous = reading;
 }
 
 void setLedIntensity(int ledPin, int value) {
-  analogWrite(ledPin, 255-value);
+  analogWrite(ledPin, 255 - value);
 }
